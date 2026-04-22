45e7022270ea5659518e18155b44ee65435f0204
Murke1998 <murat.baydauletov.98@mail.ru>
2026-04-22 17:07:52 +0500

    test commit

--- tests/test_successful_registration.py
+++ tests/test_successful_registration.py
@@ -3,13 +3,13 @@ import pytest
 from pages.registration_page import RegistrationPage
 from pages.dashboard_page import DashboardPage
 
+@pytest.mark.registration
 @pytest.mark.regression
-@pytest.mark.authorization
-@pytest.mark.parametrize("email, username, password",[
-    ("user.name@gmail.com", "545","password"),
-    ("user.name@gmail.com", "4894","password")
+@pytest.mark.parametrize("email, username, password", [
+    ("test@gmail.com", "testuser", "Password123"),
 ])
-def test_successful_registration(registration_page: RegistrationPage, dashboard_page email:str, username:str, password:str):
-    registration_page.fill_registration_form(email=email, username=username, password= password)
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage, email:str,
+                                 username:str, password:str):
+    registration_page.fill_registration_form(email, username, password)
     registration_page.click_registration_button()
-    dashboard_page.
+    dashboard_page.visible_dashboard_title_text()
